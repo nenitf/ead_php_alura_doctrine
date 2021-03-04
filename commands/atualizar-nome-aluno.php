66da88d2cf36a2c8cfb5c1e9e577312d987c2c51
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 18:52:29 -0300

    :tv: add aula 7

--- commands/atualizar-nome-aluno.php
+++ commands/atualizar-nome-aluno.php
@@ -1,5 +1,8 @@
 <?php
 
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
