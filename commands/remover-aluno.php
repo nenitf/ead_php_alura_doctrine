a6ea0f89371357555631145fbc1d2b13e1b13f7d
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 18:59:30 -0300

    :tv: add aula 8

---
+++ commands/remover-aluno.php
@@ -0,0 +1,18 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$id = $argv[1];
+
+$aluno = $entityManager->getReference(Aluno::class, $id); // + perf, não popula aluno
+var_dump($aluno);die;
+// $aluno = $entityManager->find(Aluno::class, $id);
+
+$entityManager->remove($aluno);
+$entityManager->flush();
