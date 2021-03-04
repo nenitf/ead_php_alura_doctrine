c640b686931c17fbec1d9b1b5ab2a6e1ff674888
Felipe Silva <felipe.dacs@gmail.com>
2021-03-04 18:51:25 -0300

    :tv: add aula 6

---
+++ commands/atualizar-nome-aluno.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+$alunoRepository = $entityManager->getRepository(Aluno::class);
+
+// php commands\atualizar-nome-aluno.php 1 "João Silva"
+$id = $argv[1];
+$novoNome = $argv[2];
+
+$aluno = $alunoRepository->find($id);
+$aluno->setNome($novoNome);
+
+$entityManager->flush();
+
